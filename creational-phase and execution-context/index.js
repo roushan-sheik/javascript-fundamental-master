05942a550d51493e2297ff3de86cbf2a82b7da00
roushan-sheik <roushansheik@gmail.com>
2024-01-31 19:21:17 +0600

    creational phase and execution context added

--- creational-phase and execution-context/index.js
+++ creational-phase and execution-context/index.js
@@ -1,1 +1,18 @@
 let a = "Arifa moni";
+//function invoking
+printName();
+let b;
+console.log(b);
+b = "Tasmiya Jahan";
+//function declaration
+function printName() {
+  console.log("Md Rowshan Sheikh");
+}
+//* creational phase
+// a = undefined
+// b = undefined
+// printName = ref
+//* execution phase
+// print a = "Arifa Moni"
+// print printName() clg() 
+// print b = undefined
