58e9bfd4e20bb036f61215f25eb16d9079ca8839
roushan-sheik <roushansheik@gmail.com>
2024-02-06 22:24:53 +0600

    Replace all occurrences of a string

---
+++ array/replace.js
@@ -0,0 +1,6 @@
+// Replace all occurrences of a string
+const quote =
+  "React is a JS framework & this framework is the most popular front-end framework right now";
+
+// Replace all occurrences of 'framework' with 'library'
+console.log(quote.replace(/framework/g, "library")); // React is a JS library & this library is the most popular front-end library right now
