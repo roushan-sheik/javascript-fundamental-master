fd2208012951fa43b37670f3f136f076adf18cb8
roushan-sheik <roushansheik@gmail.com>
2024-02-06 21:56:16 +0600

    another way to convert string to boolean

--- truthy value vs falsey value/conver-to-boolean.js
+++ truthy value vs falsey value/conver-to-boolean.js
@@ -1,7 +1,9 @@
 // Convert string to boolean value
 // Print output will be = true
 let named = "Md Rowshan Sheikh";
-console.log( named );
+console.log(named);
 // another way
 let named2 = !!"Md Rowshan Sheikh";
 console.log(named2);
+let result = Boolean(named2);
+console.log(result);
