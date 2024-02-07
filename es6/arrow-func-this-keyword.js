e2b607e00b0cc62701a6a7aece9209d872ed8be0
roushan-sheik <roushansheik@gmail.com>
2024-02-07 07:40:43 +0600

    arrrow function with this

---
+++ es6/arrow-func-this-keyword.js
@@ -0,0 +1,26 @@
+const sum = (a, b) => a + b;
+console.log(sum(5, 10));
+const sqr = (x) => x * x;
+console.log(sqr(10));
+// NOTE - Lets talk about this keyword
+// If we use fat arrow function this refer the window object
+// But if we use normal function this will refer the parent object.
+let obj = {
+  named: "Arifa Moni",
+  print: () => {
+    console.log("Name is ", this.named);
+  },
+};
+obj.print();
+
+let obj2 = {
+  named: "Arifa Moni",
+  print: function () {
+    let self = this;
+    setTimeout(function () {
+      console.log("Name is ", self.named);
+    }, 1000);
+  },
+};
+
+obj2.print();
