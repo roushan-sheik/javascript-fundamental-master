9d14d32d02eee55c683ec02ea05cc166b0c419bf
roushan-sheik <roushansheik@gmail.com>
2024-02-07 08:06:03 +0600

    If we use default parameter we cannot provide ( null)

--- es6/default-parameter.js
+++ es6/default-parameter.js
@@ -3,3 +3,10 @@ function Sum(a = 5, b = 10) {
   console.log(a + b);
 }
 Sum();
+// If we use default parameter we cannot provide ( null)
+// as a parameter this is throw an error.
+function greet(name = "Tasmiya", msg = "Hello") {
+  console.log(msg, name);
+  console.log(name.length);
+}
+greet(undefined, null);
