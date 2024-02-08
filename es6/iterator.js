f0ddbba084623e777034601f924d05040bd4a832
roushan-sheik <roushansheik@gmail.com>
2024-02-08 16:54:56 +0600

    Symbol dot iterator in es6

--- es6/iterator.js
+++ es6/iterator.js
@@ -1,7 +1,6 @@
 let myArr = [1, 2, 3, 4];
 // Es5 iterator
-function makeIterator ( val )
-{
+function makeIterator(val) {
   let i = 0;
   return {
     next() {
@@ -17,4 +16,12 @@ console.log(iterate.next());
 console.log(iterate.next());
 console.log(iterate.next());
 console.log(iterate.next());
-console.log(iterate.next());
+console.log( iterate.next() );
+// Symbol dot iterator in es6 
+let iterat = myArr[Symbol.iterator]();
+console.log(iterat.next());
+console.log(iterat.next());
+console.log(iterat.next());
+console.log(iterat.next());
+console.log(iterat.next());
+console.log(iterat.next());
